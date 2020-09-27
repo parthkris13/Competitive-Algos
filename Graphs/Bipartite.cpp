4bf6a6d7a12ef1a99067663a34c06cd9687685ca
parthkris13 <35499937+parthkris13@users.noreply.github.com>
2020-09-28 01:41:42 +0530

    bipartite coloring

---
+++ Graphs/Bipartite.cpp
@@ -0,0 +1,107 @@
+// Prim Algorithm for finding Maximum Spanning Tree (MST)
+// Prim Agorithm has similar ideas to Dijkstra
+// Time Complexity: O(ElogV)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1 << 30;
+const int MAX_N = 100000 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+
+int V, E, visited[MAX_N] = {0};
+vector<pair<int, int> > adj[MAX_N];
+priority_queue<ii, vii, greater<ii> > pq;
+
+void process(int u) {
+    visited[u] = 1;
+    for(int i = 0; i < adj[u].size(); i++) {
+        int v = adj[u][i].first, w = adj[u][i].second;
+        if (!visited[v]) pq.push({w, v});
+    }
+}
+
+int Prim(int s) { //return mst_cost
+    int mst_cost = 0;
+    process(s);
+    while(!pq.empty()) {
+        pair<int, int> next = pq.top(); pq.pop();
+        int w = next.first, v = next.second;
+        if (!visited[v]) {
+            mst_cost += w;
+            process(v);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1 << 30;
+const int MAX_N = 100000 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+
+int V, E;
+vector<int> adj[MAX_N];
+
+bool isBipartite() { //applying BFS and assigning colour to each node
+    queue<int> Q; int colour[V];
+    memset(colour, -1, sizeof colour);
+    Q.push(0); colour[0] = 0;
+    
+    while(!Q.empty()) {
+        int u = Q.front(); Q.pop();
+        for(int i = 0; i < adj[u].size(); i++) {
+            int v = adj[u][i];
+            if(colour[v] == -1) {
+                colour[v] = 1 - colour[u];
+                Q.push(v);
+            }
+            else if(colour[v] == colour[u]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+  
+    cin >> V >> E;
+    for(int i = 0; i < E; i++) {
+        int x, y; cin >> x >> y;
+        adj[x].push_back(y); adj[y].push_back(x);
+    }
+}
+    cin >> V >> E;    
+    for(int i = 0; i < E; i++) {
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back({v, w}); 
+        adj[v].push_back({u, w});
+    }
+    
+    cout << Prim(0);
+}
